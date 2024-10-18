89505d9e477696741e58e7490ea2775c0ac22ebd
Chakir-Aicha <134855396+Chakir-Aicha@users.noreply.github.com>
2024-10-18 21:36:51 +0100

    Create HellloWorld.java

---
+++ HellloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Affiche le message "Hello, World!" dans la console
+        System.out.println("Hello, World!");
+    }
+}
